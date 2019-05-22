d5453c227c4edb620a13117aea25cebef3e16acb
NTU-CTLD <ctld@jimAnsibleTest.dlc-test>
2019-05-22 10:10:35 +0800

    Add setUp.sh for 1) tmux color 2) git editor

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo 'Are you going to append `export TERM=screen-256color` at the end of ~/.bashrc?'
+echo 'y for yes, n for no'
+read confirm
+if [[ $confirm == 'y' ]]; then
+    echo 'export TERM=screen-256color' >> ~/.bashrc
+fi
+
+
+echo 'Are you going to set vim as your git commit editor in ~/.bashrc?'
+echo 'y for yes, n for no'
+read confirm
+if [[ $confirm == 'y' ]]; then
+    echo -e 'export VISUAL=vim\nexport EDITOR="$VISUAL"' >> ~/.bashrc
+fi
